01bd3ef62587a7384b3e7cc60bf833f8f8e38527
Morev Max <door0172@gmail.com>
2019-03-02 15:05:29 +0300

    init react-redux & react-thunk

---
+++ frontend-app/src/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { BrowserRouter } from 'react-router-dom';
+
+import {
+  createStore, compose, applyMiddleware, combineReducers,
+} from 'redux';
+
+import { Provider } from 'react-redux';
+
+import thunk from 'redux-thunk';
+
+import authorizationReducer from './reducers/authorization';
+
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const rootReducer = combineReducers({
+  authorization: authorizationReducer,
+});
+
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk)),
+);
+/* eslint-enable */
+
+ReactDOM.render((
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+), document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ frontend-app/src/reducers/authorization.js
@@ -0,0 +1,11 @@
+const initialState = {
+  userId: 5,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default: return state;
+  }
+};
+
+export default reducer;
