634c6fea0dbf7175d19be2974eb715ae6116ef76
Morev Max <door0172@gmail.com>
2019-04-07 13:14:57 +0300

    HiddenInput

--- frontend-app/src/components/Inputs/HiddenInput/HiddenInput.jsx
+++ frontend-app/src/components/Inputs/HiddenInput/HiddenInput.jsx
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class HiddenInput extends Component {
+  static defaultProps = {
+    value: null,
+  };
+
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: props.name,
+      value: props.value,
+    };
+  }
+
+  componentDidMount() {
+    const { onStateChange } = this.props;
+    onStateChange(this.state);
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    const { value } = this.state;
+    const { onStateChange } = this.props;
+    if (prevState.value !== value) {
+      onStateChange(this.state);
+    }
   }
 
   render() {
-    return <div>Hidden Input</div>;
+    return <div />;
   }
 }
 
 export default HiddenInput;
+
+HiddenInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+
+  onStateChange: PropTypes.func.isRequired,
+};
